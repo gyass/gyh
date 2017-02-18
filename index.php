8ad6950d4409bc27c615464190c58930c11ec385
SamirDjelal <samir.djelal.webdesign@gmail.com>
2017-02-18 19:37:18 +0100

    main structure

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1">
+        <title>xLeecher</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/app.js"></script>
+        <link rel="stylesheet" href="css/main.css"> 
+    </head>
+    <body>
+        <p>Hello world! This is CaZaNoVa163-HTML5 Boilerplate.</p>
+    </body>
+</html>
